34a2dfc94e2ec9c240064c66d988c5d7bbe66ae9
Garyson <bbt1250912@gmail.com>
2021-12-05 00:59:55 +0900

    이건 나도 모르겠다

--- CPMS/CPMSproject/CPMSapp/admin.py
+++ CPMS/CPMSproject/CPMSapp/admin.py
@@ -1,3 +1,7 @@
+from .models import MyTicket, Profile
 from django.contrib import admin
 
+# from .models import Profile, MyTicket
+
 # Register your models here.
+admin.site.register([Profile, MyTicket])
--- CPMS/CPMSproject/CPMSapp/models.py
+++ CPMS/CPMSproject/CPMSapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+class Profile(models.Model):
+    objects = models.Manager
+    userName = models.CharField(max_length=10)
+    userID = models.CharField(max_length=20)
+    userPassword = models.CharField(max_length=30)
+
+class MyTicket(models.Model):
+    # Profile 데이터베이스랑 종속
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        null = False,
+    )
+    objects = models.Manager
+    myTicket = ArrayField(ArrayField(models.TextField()))
+    myPaymentDetails = ArrayField(ArrayField(models.TextField()))
