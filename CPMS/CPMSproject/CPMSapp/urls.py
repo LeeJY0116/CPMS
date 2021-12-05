49398729f67e4868a1187af417d2f3ca66f85644
yubin <vundle34@naver.com>
2021-12-06 03:30:26 +0900

    로그인 DB연동

--- CPMS/CPMSproject/CPMSapp/urls.py
+++ CPMS/CPMSproject/CPMSapp/urls.py
@@ -3,6 +3,6 @@ from . import views, models
 
 urlpatterns = [
     path('', views.cpms_main, name='not_login'),
-    path('login/', views.cpms_login),
-    path('<str:user_name>/', views.cpms_main_login)
+    path('login/', views.cpms_login, name='trying_login'),
+    path('login_success/<str:user_name>/', views.cpms_main_login, name='login_of_user_name'),
 ]
--- CPMS/CPMSproject/CPMSapp/views.py
+++ CPMS/CPMSproject/CPMSapp/views.py
@@ -7,6 +7,25 @@ def cpms_main(request):
     return render(request, 'index.html', {'bool_logIO':False})
 
 def cpms_login(request):
+    if request.method == 'POST':
+        # 아이디 체크
+        try:
+            id = request.POST['user_id']
+            user = Profile.objects.get(userID=id)
+        except:
+            return render(request, 'login.html', {'str_error':'존재하지 않는 ID입니다.'})
+        # 비밀번호 체크
+        try:
+            password = request.POST['user_password']
+            user = Profile.objects.filter(userID=id).get(userPassword=password)
+        except:
+            return render(request, 'login.html', {'str_error':'비밀번호가 잘못되었습니다.'})
+        
+        user.bool_logIO = True  # 로그인 변수 True로 변경
+        user.save()     # Update후, save() 필수!
+        user_name = user.userName   # 유저 닉네임
+        return redirect('login_of_user_name', user_name)
+
     return render(request, 'login.html')
 
 def cpms_main_login(request, user_name):
